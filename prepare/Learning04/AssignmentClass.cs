c90fafb28ca1633ccac8af8b6f67024357258395
josep rivera <eliezerive96@gmail.com>
2024-05-19 22:39:16 -0600

    New updates Learning 04

---
+++ prepare/Learning04/AssignmentClass.cs
@@ -0,0 +1,21 @@
+public class AssignmentClass
+{
+
+
+    protected string _studnetName;
+
+    private string _topic;
+
+    public AssignmentClass(string name, string topic)
+    {
+        _studnetName = name;
+        _topic = topic;
+
+    }
+
+
+    public string GetSummary()
+    {
+        return $"{_studnetName} - {_topic}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Console.WriteLine("Hello Learning04 World!");
+
+
+        AssignmentClass newAssignment = new AssignmentClass("Josep", "Functions");
+        Console.WriteLine(newAssignment.GetSummary());
+        Console.WriteLine();
+
+
+        MathAssignment math1 =  new MathAssignment("Eliezer Rivera", "Functions", "7.5", "8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+        Console.WriteLine();
+        
+        WrittingAssignment writting1 = new WrittingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writting1.GetSummary());
+        Console.WriteLine(writting1.GetWritingInformation());
     }
 }
